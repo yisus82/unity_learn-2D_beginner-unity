bbf4e452f2ffcfe542a4f1e7ac34ffca52b545af
yisus82 <djalma8@gmail.com>
2024-10-29 13:30:57 +0100

    Read keyboard input

--- Ruby's Adventure/Assets/Scripts/PlayerController.cs
+++ Ruby's Adventure/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,29 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     private void Update()
     {
+        float horizontal= 0.0f;
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            horizontal = -1.0f;
+        } else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            horizontal = 1.0f;
+        }
+        float vertical = 0.0f;
+        if (Keyboard.current.downArrowKey.isPressed)
+        {
+            vertical = -1.0f;
+        } else if (Keyboard.current.upArrowKey.isPressed)
+        {
+            vertical = 1.0f;
+        }
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 0.1f * horizontal;
+        position.y = position.y + 0.1f * vertical;
         transform.position = position;
     }
 }
