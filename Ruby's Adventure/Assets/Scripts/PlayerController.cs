49f7323c1b517d66d98dd6756c55fda82d7e5e73
yisus82 <djalma8@gmail.com>
2024-11-01 13:30:33 +0100

    Player's health system

--- Ruby's Adventure/Assets/Scripts/PlayerController.cs
+++ Ruby's Adventure/Assets/Scripts/PlayerController.cs
@@ -1,28 +1,38 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
+    public int maxHealth = 5;
     public float speed = 3.0f;
     [FormerlySerializedAs("MoveAction")] public InputAction moveAction;
     
-    private Rigidbody2D rigidbody2d;
-    private Vector2 movement;
+    private Rigidbody2D _rigidbody2d;
+    private Vector2 _movement;
+    private int _currentHealth;
     
     private void Start()
     {
         moveAction.Enable();
-        rigidbody2d = GetComponent<Rigidbody2D>();
+        _rigidbody2d = GetComponent<Rigidbody2D>();
+        _currentHealth = maxHealth;
     }
     
     private void Update()
     {
-        movement = moveAction.ReadValue<Vector2>();
+        _movement = moveAction.ReadValue<Vector2>();
     }
     
     private void FixedUpdate()
     {
-        rigidbody2d.MovePosition(rigidbody2d.position + movement * (speed * Time.fixedDeltaTime));
+        _rigidbody2d.MovePosition(_rigidbody2d.position + _movement * (speed * Time.fixedDeltaTime));
+    }
+    
+    public void ChangeHealth(int amount)
+    {
+        _currentHealth = Mathf.Clamp(_currentHealth + amount, 0, maxHealth);
+        Debug.Log(_currentHealth + "/" + maxHealth);
     }
 }
